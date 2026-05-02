a0feda73d2216ad75e642ead2d098b171c781c6b
gamesove132 <gamesove132@gmail.com>
2026-05-02 08:24:03 +0300

    Update PvpCommand.java

--- src/main/java/com/Pvpnew/Command/PvpCommand.java
+++ src/main/java/com/Pvpnew/Command/PvpCommand.java
@@ -1,6 +1,6 @@
-package com.Pvpnew.Command;
+package com.pvpsystem.commands;
 
-import com.Pvpnew.PvpPlugin;
+import com.pvpsystem.PvpPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
